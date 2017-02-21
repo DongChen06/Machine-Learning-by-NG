7f61bbbf8fa7778731d8e80a9d4dd62919acd6e8
Derekabc <2452291964@qq.com>
2017-02-21 06:03:23 -0600

    Add files via upload

---
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,55 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+
+x1 = X(:,1);
+x2 = X(:,2);
+for i = 1:8
+    C = [0.01,0.03,0.1,0.3,1,3,10,30];
+    C = C(i);
+    for j = 1:8
+        sigma = [0.01,0.03,0.1,0.3,1,3,10,30];
+        sigma = sigma(j);
+
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+
+        EEROR_Val(i,j)= mean(double(predictions ~= yval));  
+        
+        
+    end
+end
+C = [0.01,0.03,0.1,0.3,1,3,10,30];
+sigma = [0.01,0.03,0.1,0.3,1,3,10,30];
+ [m n]=find(EEROR_Val==min(min(EEROR_Val)));
+ C = C(m);
+ sigma = sigma(n);
+
+
+
+
+% =========================================================================
+
+end
